2415f8c943c40b94b2e62db2dae149ce3b6b6519
anon1689501 <95393162+anon1689501@users.noreply.github.com>
2022-11-12 13:43:22 -0600

    day 1 complete

--- day1.go
+++ day1.go
@@ -1,10 +1,46 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"log"
+	"os"
+	"strconv"
 )
 
 func main() {
 
-	fmt.Println("quote")
+	day1, err := os.Open("inputs/day1.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer day1.Close()
+
+	scanner := bufio.NewScanner(day1)
+	total := 0
+	for scanner.Scan() {
+		// myNum, err2 := scanner.Text()
+		myNum, err2 := strconv.Atoi(scanner.Text())
+
+		total = total + fuel(myNum)
+		fmt.Println(total)
+		if err2 != nil {
+			log.Fatal(err)
+		}
+
+	}
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	// fmt.Println(total)
+}
+
+func fuel(x int) int {
+	x = (x / 3) - 2
+	if x < 0 {
+		return 0
+	}
+	return x + fuel(x)
+
 }
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/anon1689501/AOC2021go
+
+go 1.19
